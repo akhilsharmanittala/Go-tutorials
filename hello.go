87857e8a36cbf691a991ab4a051689f0f7d3f560
akhilsharmanittala <53893678+akhilsharmanittala@users.noreply.github.com>
2025-08-18 19:18:39 +0530

    static and dynamically prints greeting message

--- hello.go
+++ hello.go
@@ -1,14 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func hello_world() {
 	fmt.Println("Hello, World! :)")
 	fmt.Println("Welcome to Go programming!, this is my first program.")
 }
 
+// here name is a static string
+// this function prints a greeting with a static name
+func hello_static_name(name string) {
+	fmt.Println("Hello, " + name + "!")
+}
+
+// accepts name from user input and prints a greeting
+func hello_dynamic_name() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your name: ")
+	name, _ := reader.ReadString('\n') // Read input until a newline
+	fmt.Println("Hello, " + name + "!")
+}
+
+// main function to execute the program
 func main() {
 	hello_world()
+	hello_static_name("Akhil")
+	fmt.Println("This is a simple Go program demonstrating basic output.")
+	hello_static_name("Jhaishna")
+	fmt.Println("Goodbye!")
+	hello_static_name("Lavanya")
+	hello_dynamic_name()
 }
 
 // This program prints "Hello, World!" to the console.
