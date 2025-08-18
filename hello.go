45d9ef0c0d4dbd54038818c78604e89e5d3e9a07
akhilsharmanittala <53893678+akhilsharmanittala@users.noreply.github.com>
2025-08-18 19:01:48 +0530

    Create hello.go

---
+++ hello.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func hello_world() {
+	fmt.Println("Hello, World! :)")
+	fmt.Println("Welcome to Go programming!, this is my first program.")
+}
+
+func main() {
+	hello_world()
+}
+
+// This program prints "Hello, World!" to the console.
+// It also includes a welcome message for Go programming.
